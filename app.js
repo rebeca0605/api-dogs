a250070d246324ed23570dd1d2d6044784a5cea9
REBECA GOMES DA SILVA <25132910@CFP127ALUNO.LOCAL>
2025-09-16 08:50:31 -0300

    Header finalizado

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 'use strict'
 
+const botaoBuscar = document.getElementById('botao-buscar')
+const input = document.getElementById('inputRaca')
+const galeria = document.getElementById('galeria')
+
 async function buscarImagens(raca) {
     const url = `https://dog.ceo/api/breed/${raca}/images`
     const response = await fetch (url)
     const imagens = await response.json()
+
     console.log(imagens.message)
     return imagens.message
 }
 
-buscarImagens('doberman')
+async function exibirImagens(){
+    const galeria = document.getElementById("galeria")
+    const raca = document.getElementById('inputRaca').value.toLowerCase()
+
+    galeria.innerHTML = ""
+
+    const listaImg = await buscarImagens(raca)
+
+    listaImg.slice(0,20).forEach(url => {
+        const img = document.createElement("img")
+        img.src = url
+        galeria.appendChild(img)
+    })
+}
+
+botaoBuscar.addEventListener('click', exibirImagens)
